d3a16ff01918c09ff278397d705c2770a15e1ed9
Hirusha2000 <2000hirushaudara@gmail.com>
2024-04-20 00:15:35 +0530

    file uploading

---
+++ img/src/main/java/com/example/img/ImgApplication.java
@@ -0,0 +1,15 @@
+package com.example.img;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ImgApplication.class, args);
+		System.out.println("welcome to the spring boot project");
+	}
+
+}
